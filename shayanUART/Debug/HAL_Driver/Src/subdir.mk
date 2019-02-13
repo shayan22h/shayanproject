48a2da9749c109894849f95ea6947e6cf683f0f3
shayan <shayan13h@gmail.com>
2019-02-13 23:51:58 +0100

    Commited

--- shayanUART/Debug/HAL_Driver/Src/subdir.mk
+++ shayanUART/Debug/HAL_Driver/Src/subdir.mk
@@ -273,7 +273,7 @@ HAL_Driver/Src/%.o: ../HAL_Driver/Src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Compiler'
 	@echo $(PWD)
-	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429ZITx -DNUCLEO_F429ZI -DDEBUG -DSTM32F429xx -DUSE_HAL_DRIVER -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/HAL_Driver/Inc/Legacy" -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/Utilities/STM32F4xx_Nucleo_144" -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/inc" -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/CMSIS/device" -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/CMSIS/core" -I"C:/Users/shaya/OneDrive/Desktop/Portfolio/freertos/shayanUART/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F429ZITx -DNUCLEO_F429ZI -DDEBUG -DSTM32F429xx -DUSE_HAL_DRIVER -I"C:/Users/shaya/git/shayanproject/shayanUART/HAL_Driver/Inc/Legacy" -I"C:/Users/shaya/git/shayanproject/shayanUART/Utilities/STM32F4xx_Nucleo_144" -I"C:/Users/shaya/git/shayanproject/shayanUART/inc" -I"C:/Users/shaya/git/shayanproject/shayanUART/CMSIS/device" -I"C:/Users/shaya/git/shayanproject/shayanUART/CMSIS/core" -I"C:/Users/shaya/git/shayanproject/shayanUART/HAL_Driver/Inc" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
